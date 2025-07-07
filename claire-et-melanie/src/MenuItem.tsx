a81f25047afa45be6ad9670978ee33703760fd9b
seb776 <sebastien.maire@epitech.eu>
2025-07-07 23:17:32 +0200

    Fixed submenu item positionning for wix

--- claire-et-melanie/src/MenuItem.tsx
+++ claire-et-melanie/src/MenuItem.tsx
@@ -37,7 +37,7 @@ export default function MenuItem(props: IMenuItemProps) {
         }
     }, [])
 
-    const RADIUS = 40;
+    const RADIUS = 35;
     async function onClickButton() {
         if (refAudio.current) {
             if (!props.linkTo) // avoid cutting the sound when page redirects 
@@ -58,7 +58,7 @@ export default function MenuItem(props: IMenuItemProps) {
                         const elem = document.getElementById("subCategory" + i + props.image);
                         elem?.animate([
                             { top: '0vh', left: '0vw' },
-                            { top: `calc(${Math.sin(categories[i].angle) * RADIUS}vh)`, left: `calc(${Math.cos(categories[i].angle) * RADIUS}vw)` },
+                            { top: `calc(${Math.sin(categories[i].angle) * RADIUS}vh*0.75)`, left: `calc(${Math.cos(categories[i].angle) * RADIUS}vw)` },
                         ], { duration: 500, iterations: 1, fill: 'forwards', easing: 'ease-out' })
                     }
                 }
@@ -86,9 +86,9 @@ export default function MenuItem(props: IMenuItemProps) {
         {<div style={{ position: 'absolute' }}>
             {categories && categories.map((el, i) =>
                 <div id={"subCategory" + i + props.image} style={{ visibility: selected ? 'visible' : 'hidden', position: 'absolute' }}>
-                    <div onClick={() => { onClickButtonCategory(el) }} className="menuItem water-wave" style={{ cursor: 'pointer', width: "calc(max(20vw, 30vh)) ", aspectRatio: 1, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', maskPosition: 'center', maskRepeat: 'no-repeat', maskSize: 'contain', maskImage: `url(./Maskblob.png)`, backgroundImage: `url(${el.image})`, backgroundSize: 'cover' }}>
+                    <div onClick={() => { onClickButtonCategory(el) }} className="menuItem water-wave" style={{ cursor: 'pointer', width: "calc(min(50vw, 30vh)) ", aspectRatio: 1, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', maskPosition: 'center', maskRepeat: 'no-repeat', maskSize: 'contain', maskImage: `url(./Maskblob.png)`, backgroundImage: `url(${el.image})`, backgroundSize: 'cover' }}>
                     </div>
-                        <div style={{position: 'absolute', pointerEvents: 'none', fontSize: '25pt', color:'#3AFF47', fontWeight: 'bold', zIndex: 99, left: '50%', top: '50%', textAlign: 'center', textWrap: 'wrap', transform: 'translate(-50%, -50%)', filter: 'drop-shadow(0.35rem 0.35rem 0.4rem rgba(0, 0, 0, 0.5))'}}>
+                        <div style={{position: 'absolute', pointerEvents: 'none', fontSize: '20pt', color:'#3AFF47', fontWeight: 'bold', zIndex: 99, left: '50%', top: '50%', textAlign: 'center', textWrap: 'wrap', transform: 'translate(-50%, -50%)', filter: 'drop-shadow(0.35rem 0.35rem 0.4rem rgba(0, 0, 0, 0.5))'}}>
                             {el.title}
                         </div>
                 </div>
